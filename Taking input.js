e8d5357234505488287897cacf22bdbb9de1029a
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-10-25 20:43:52 +0600

    Create 6. Taking input.js

---
+++ Taking input.js
@@ -0,0 +1,24 @@
+/*let username ;
+username = window.prompt("Whats your username ?");
+console.log(username);
+
+let username;
+
+username = window.prompt("What is your username?");
+console.log(username);
+
+
+
+/*document.getElementById("mySubmit").onclick=function(){
+username= document.getElementById("myText").value;
+
+console.log(username)
+}
+*/
+
+let username;
+
+document.getElementById("mySubmit").onclick = function(){
+    username = document.getElementById("myText").value;
+    console.log(username);
+}
