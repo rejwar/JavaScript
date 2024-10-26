21c679b410d7428ba1e935eb1d7a498ed0cd0665
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-10-27 03:19:56 +0600

    Create Data Type.js

---
+++ Data Type.js
@@ -0,0 +1,14 @@
+
+let firstName = 'Ali';
+firstName = 'shuvo'
+let fullName = "My name is 'Ali'";
+let finalName = `My name is ${firstName}`;
+
+let age =30;
+let age2 = 35;
+let calculation = age+age2+firstName
+
+console.log(fullName);
+console.log(typeof firstName);
+console.log(finalName)) 
+console.log(calculation);
