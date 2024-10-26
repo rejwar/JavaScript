f864ad7bbb2efe7c18c51b32c4d9ebc5d5257138
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-10-26 13:01:50 +0600

    Create Statements.js
    Learning statements from Procoder Bd

---
+++ Statements.js
@@ -0,0 +1,20 @@
+ console.log("hello");
+
+document.getElementById('title').innerHTML="Hello world";
+
+sum = 50+140;
+
+console.log(sum);
+
+if (sum>100){
+    console.log('Expensive');
+}
+else{
+    console.log('Cheap');
+}
+
+function sayHello(name){
+    console.log(`hello &{name}`);
+}
+
+sayHello('shuvo');
