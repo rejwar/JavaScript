f7845e22e8692667d1cdfc10a161f59ee9839d3b
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-10-26 14:39:40 +0600

    Create 10. Syntex.js
    Syntex learning from Procoder Bd

---
+++ 10. Syntex.js
@@ -0,0 +1,26 @@
+let firstName = "Rifat";
+
+let name ="30";
+
+/*firstName
+
+FirstName
+
+phoneNumber 
+
+firstname
+*/
+
+firstName = "shuvo";
+
+console.log(firstName);
+
+
+console.log("Hello");
+console.log("World");
+
+function funcName(){
+    console.log("My Fisrt Function");
+}
+
+funcName();
