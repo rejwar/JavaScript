0c5b41bc81f498c57762fd128a84e248aab053f3
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-10-28 20:54:15 +0600

    Create Operator Presendence.js
    Learning of js

---
+++ Operator Presendence.js
@@ -0,0 +1,11 @@
+const now =2037;
+
+const ageJones = now -1991;
+const ageRejwar = now -2001;
+console.log(now -1991 > now -2018);
+console.log(20-10-5);
+let x,y;
+x=y=25-15-5;
+console.log(x,y);
+const averageAge = ageJones+ageRejwar;
+console.log(ageJones,ageRejwar,averageAge);
