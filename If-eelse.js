bc58b9a44cab832b1426eaef8f8eea57b96cc94d
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-10-29 02:11:16 +0600

    Create If-eelse.js

---
+++ If-eelse.js
@@ -0,0 +1,45 @@
+// if else 
+
+/*const age =19;
+const isOLdEnough = age=>18;
+
+if (isOLdEnough)
+{
+    console.log(`Sarah can start a car`);
+}
+    */
+
+/*const age =24;
+if(age <19)
+{
+    console.log(`she can't drive a car`);
+}
+
+else
+{
+    console.log(`yes she can `);
+}
+*/
+
+
+
+/*const age =15;
+if(age>=25){
+    console.log(`yes she can drive `);
+}
+else{
+    const yearsLeft = 18-age;
+    console.log(`she has to wait for ${yearsLeft}`);
+}
+*/
+
+const birthYear=2012;
+if (birthYear <= 2000)
+{
+    century =20;
+}
+else
+{
+    century =21;
+}
+console.log(century);
