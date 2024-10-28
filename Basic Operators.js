411eeba1784ab10305a6ea74daa0cfdeb160b5e0
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-10-28 20:52:34 +0600

    Create Basic Operators.js
    learning practice of Java script

---
+++ Basic Operators.js
@@ -0,0 +1,26 @@
+const now =2037;
+const ageRejwar = 2024-2001;
+const ageSarah = 2025-2001;
+
+console.log(ageRejwar,ageSarah);
+console.log(ageRejwar*2 ,ageRejwar/2);
+
+const firstName='Md. Rejwar';
+const lastName=' Rifat';
+
+console.log(firstName + ' '+lastName);
+
+let x = 110 + 5;
+x+=10;
+x*=3;
+x++;
+x--;
+console.log(x);
+
+//comparison operator: 
+
+console.log(ageRejwar>ageSarah);
+console.log(ageSarah>=18);
+
+const isFullAge=Sarah>=18;
+console.log(isFullAge);
