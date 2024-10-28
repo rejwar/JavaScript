04ddfd50379439ac305e32664580135dad55773e
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-10-28 15:59:40 +0600

    Create  Data Types.js
    Practising from Jonas Schmedtmann's  lecture

---
+++ Data Types.js
@@ -0,0 +1,57 @@
+let js = 'amazing'
+if (js==='amazing') alert("Java script is FUN");
+ 
+console.log(40+8+9+10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName="Rejwar"
+console.log(firstName);
+
+let firstNamea=`bob`;
+console.log(firstNamea);
+
+//let 3years= 3;
+
+//let jonas&matila='jtm'
+
+//let new = 27;
+//variable name convention
+
+let _function=30;
+    console.log(_function);
+
+let myFirstJob ="Failure"
+let myCurrentJob ="Looser"
+
+let job1 ="failure"
+let job2 ="looser"
+
+console.log(myFirstJob);
+
+true;
+console.log(true);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log((typeof 23));
+console.log(typeof "Rejwar");
+
+javascriptIsFun = 'YES !';
+console.log(javascriptIsFun);
+
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year)
+console.log(typeof year);
+
+year= 1991;
+
+console.log(year);
+
+console.log(typeof year);
+console.log(null);
