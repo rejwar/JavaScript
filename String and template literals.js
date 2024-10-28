ec55304e83b65b50c110463e19dfc6406fccd978
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-10-28 21:40:26 +0600

    Create String and template literals.js

---
+++ String and template literals.js
@@ -0,0 +1,11 @@
+const firstName= 'Jones';
+const job = 'teacher';
+const birthYear = 1993;
+const year = 1997;
+
+const jonas = "I'm "+firstName +" ,a " +(year-birthYear)+'year old' + job +'!'
+
+console.log(jonas);
+
+const newJonas = `I'am ${firstName}, a ${birthYear} , b ${year}`
+console.log(`this is a new line \n \ hi `);
