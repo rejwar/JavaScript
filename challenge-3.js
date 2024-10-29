25c6bacfb8614e80e48679165866b2e132208df6
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-10-29 13:44:15 +0600

    Create challenge-3.js

---
+++ challenge-3.js
@@ -0,0 +1,19 @@
+const scoreDolphins = (96 + 108 + 89)/3;
+const scoreKoalas = (88+89+99)/3;
+console.log(scoreDolphins,scoreKoalas);
+
+if(scoreDolphins>scoreKoalas);
+{
+    console.log(`Dolphines own the match `);
+}
+
+else if (scoreKoalas>scoreDolphins);
+{
+    console.log(`Koala own the match`);
+}
+
+else if (scoreDolphins===scoreKoalas);
+{
+
+console.log(`Both own the match`);
+}
