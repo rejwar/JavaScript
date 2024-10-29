5c3e26918416917466fa07a4affd7063fe4a43f4
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-10-29 11:34:36 +0600

    Create truthy-falsy.js

---
+++ truthy-falsy.js
@@ -0,0 +1,25 @@
+// truthy & falsy 
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean(true));
+console.log(Boolean(false));
+
+var money = 0;
+if( money=10){
+
+    console.log(`Don't spend all money`);
+}
+
+else{
+    console.log(`You should get a job `);
+}
+
+let height ;
+if (height)
+{
+    console.log(`Height is defined`);
+}
+else
+{
+    console.log(`Height is not defined`);
+}
