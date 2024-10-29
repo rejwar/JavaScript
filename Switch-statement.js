8c33a092b2046113e1fa0344c5b40d95203baec3
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-10-30 02:02:39 +0600

    Create Switch-statement.js

---
+++ Switch-statement.js
@@ -0,0 +1,28 @@
+const day = 'Monday';
+
+switch (day){
+    case 'Monday':
+        console.log(`Hei how are you  nsdfgk?`);
+        break;
+
+    case 'Tuesday':
+        console.log("bahar nikal motherc");
+        break;
+
+        case 'wednesday':
+        console.log(`Hei how are you ?`);
+        break;
+
+        case 'Thursday':
+        console.log(`Hei how are you ?`);
+        break;
+    
+        case 'Friday':
+        console.log(`Hei how are you ?`);
+        break;
+
+        default:
+            console.log('Go and die');
+}
+
+
