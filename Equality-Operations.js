ea6717748b948a6b5f4c2d270db7db0e4c31aaf3
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-10-29 12:00:16 +0600

    Create Equality-Operations.js

---
+++ Equality-Operations.js
@@ -0,0 +1,28 @@
+//equality operations
+
+const age =18;
+if(age===8);
+
+    console.log(`you just become an adult`);
+
+const favourite =prompt("What is your prompt ?");
+console.log(favourite);
+
+console.log(typeof favourite);
+
+if (favourite =23)
+{
+    console.log('cool ! is amazing number ');
+}
+
+else if (favourite ===7)
+{
+    console.log(`7 is also a cool`);
+}
+else
+{
+    console.log(`Number is not 23 or 7`);
+}
+
+if ( favourite != 23)
+    console.log(`Why not 23 `);
