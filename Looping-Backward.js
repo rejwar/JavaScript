dd6864980ee87572edfa9d3a887813c88ad040fb
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-11-01 00:22:01 +0600

    Create Looping-Backward.js

---
+++ Looping-Backward.js
@@ -0,0 +1,23 @@
+const jonas =[
+    'Jonas',
+    'Rejwar',
+    2037-2002,
+    'teacher',
+    ['Michael','Peter','Steven']
+];
+
+for (let i = jonas.length-1;  i>=0 ; i--)
+    {
+console.log(i,jonas[i]);
+}
+
+
+for (let exercise =1 ; exercise <4 ; exercise++)
+{
+    console.log(`----starting Exercise ${exercise}`);
+
+    for (let  rep  = 1 ; rep<6 ; rep++)
+    {
+        console.log(`Lifting weight repetition ${rep}`);
+    }
+}
