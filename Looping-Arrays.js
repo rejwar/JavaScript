4a63b3394a4e175d94bb0bc59c4ca9ab8370260c
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-10-31 19:16:17 +0600

    Create Looping-Arrays.js

---
+++ Looping-Arrays.js
@@ -0,0 +1,30 @@
+ const jonas = [
+    'Jonas',
+    'Rejwar',
+   ' 2037-1999',
+    'teacher',
+    ['Michael','Peter','steven'],
+    true
+];
+const type = [];
+
+
+for (let  i =0 ; i <jonas.length; i++)
+{
+    console.log(jonas[i]);
+    types[i] =typeof jonas[i];
+
+};
+
+console.log(types);
+
+const years = [1991,1992,1993,1994];
+const ages = [];
+
+for( let i= 0; i <years.length; i++ ){
+    2037 -years[i]
+}
+
+console.log(ages);
+
+
