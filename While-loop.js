ee1014ad6ab823ecad6adb5b09cbd707f63ba1e7
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-11-01 00:50:53 +0600

    Create While-loop.js

---
+++ While-loop.js
@@ -0,0 +1,18 @@
+for ( let rep =1; rep<=10; rep++)
+{
+    console.log(`lifting weights repetition ${rep}`);
+}
+
+let rep =1;
+while(rep <=10){
+    console.log(`lifting eights repetition ${rep}`);
+    rep++;
+}
+
+let dice = Math.random()*6;
+console.log(dice);
+
+while (dice!==6){
+    console.log(`You rolled a ${dice }`);
+    dice = Math. trunc(Math.random() *6)+1;
+}
