9c774e5be0e4d719ca02cfb273c5cc7308775f26
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-10-31 23:48:16 +0600

    Create Looping-Array.js

---
+++ Looping-Array.js
@@ -0,0 +1,20 @@
+const jonas = [
+
+    'Jonas' ,
+    'Rejwar',
+    '2037-2019',
+    'teacher',
+    ['Michael','Peter','Steven']
+];
+
+//for ( let i = jonas.length-1; i >= 0; i-- ){
+//    console.log(i,jonas[i])
+//}
+
+for(let exercise =1; exercise <4; exercise++ );
+console.log('--------- string exercise ${exercise}');
+
+for (let rep =1 ; rep <6; rep++)
+{
+    console.log(`Lifting rop ${rep}`);
+}
