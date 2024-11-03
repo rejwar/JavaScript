0ef1aadc6ddc95d4950f94d46f429d8a7d5bdfd1
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-11-03 23:08:56 +0600

    Create Global-scope.js

---
+++ Global-scope.js
@@ -0,0 +1,35 @@
+var globalVar = " I am global";
+function display() 
+{
+console.log(globalVar);
+}
+display();
+
+
+var globalVar2 = "I am a global Variable 00";
+function ShowGlobalVar2(){
+    console.log(globalVar2);
+}
+ShowGlobalVar2();
+console.log(globalVar2);
+
+
+
+
+var globalVar3 = " I am a global variable 3 ";
+function ShowGlobalVar3()
+{
+    console.log(globalVar3);
+}
+ShowGlobalVar3();
+console.log(globalVar3);
+
+
+
+var globalVar4 = "I am a programmer";
+function ShowGlobalVar4(){
+    console.log(globalVar4);
+}
+ShowGlobalVar4();
+console.log(globalVar4);
+
