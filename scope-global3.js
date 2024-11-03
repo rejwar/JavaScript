4f1d4d2ef91e09978096f4a719afad75627ad979
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-11-04 02:39:47 +0600

    Create scope-global3.js

---
+++ scope-global3.js
@@ -0,0 +1,14 @@
+let globalLet = " I am global with let ";
+const globalConst = "I am global with const ";
+
+function showGlobals()
+{
+console.log(globalLet);
+console.log(globalConst);
+}
+
+showGlobals();
+
+console.log(globalConst);
+console.log(globalLet);
+
