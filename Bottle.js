06f577e79f530a0653bf4820b7844a2e4557bd78
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-11-21 21:57:15 +0600

    Create Bottle.js

---
+++ Bottle.js
@@ -0,0 +1,13 @@
+function getMilk(money) {
+    console.log("leavaHoosue ");
+        console.log("leavaHoosue ");
+        console.log("loosue ");
+        console.log("leavasue ");
+
+
+    var numberOfBottles = Math.floor(money/3);
+    console.log("Buy " + numberOfBottles + "bottlesof tMilk");
+    
+}
+
+getMilk(21);
