e1fcdd4f476b8bd42e81e4f3a79a54fc1098acd4
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-11-22 00:02:24 +0600

    Create Adding.js

---
+++ Adding.js
@@ -0,0 +1,13 @@
+var number = 4;
+var anotherNumber = 5;
+
+function add(n1,n2) {
+console.log(n1+n2);    
+}
+
+function multiply(n1,n2) {
+console.log(n1*n2);    
+}
+
+add(number,anotherNumber);
+multiply(number,anotherNumber);
