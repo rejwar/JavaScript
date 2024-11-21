bc68f7ec5b0f5698e84ac91f3e8259bde80d7d31
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-11-21 10:25:42 +0600

    Create Random.js

---
+++ Random.js
@@ -0,0 +1,23 @@
+//alert("Hello");
+//alert("World");
+2+3
+
+typeof(23)
+typeof("ANgela");
+prompt("What is your name ?");
+
+var myName= "Rifat"
+typeof(myName)
+
+alert(myName)
+
+var YourName =prompt("What is your name ?");
+
+
+alert("My name is "+myName+", welcome to my course " + YourName +"!");
+
+var gameLevel = 1;
+gameLevel =2;
+gameLevel =3;
+
+alert("Your level is  currently :" + gameLevel);
