2d2441ee00b706cf4ab54bb3bbbe328395d00aeb
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-11-22 01:25:51 +0600

    Create Love.js

---
+++ Love.js
@@ -0,0 +1,8 @@
+var n = Math.random();
+n = n*6;
+ n = Math.floor(n) +1;
+console.log(n);
+
+var loveScore = Math.random()*100;
+loveScore = Math.floor(loveScore) +1;
+alert("Your love score is " + loveScore +"%");
