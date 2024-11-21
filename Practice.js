646f5b846f27ca3c28a6978119989db1c46463f4
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-11-21 20:04:35 +0600

    Create Practice.js

---
+++ Practice.js
@@ -0,0 +1,15 @@
+
+/*var tweet = prompt("Compose your tweet :");
+var tweetCount = tweet.length;
+alert("You have written" + tweetCount +"Charater , you have "+(140 - tweetCount) + "Charater reamaining ");
+
+ var tweet = prompt("Compose tweet");
+alert (prompt("Compose your tweet :").slice(0,140));
+*/
+
+var name = prompt("What is your name");
+alert("Hello " +name);
+
+
+
+
