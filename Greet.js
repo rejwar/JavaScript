b899e9fef3973221139506fb3ab475feea37c187
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-11 15:20:21 +0600

    Create Greet.js

---
+++ Greet.js
@@ -0,0 +1,42 @@
+/*const number = 5;
+
+console.log(`Multiplication Table of ${number}:`);
+for (let i = 1; i <= 10; i++) {
+  console.log(`${number} x ${i} = ${number * i}`);
+}
+  
+
+
+function greet(name, age ){
+    console.log("Hello world " + name+" "+age +"!");
+}
+greet("alice",24);
+greet("alice2" , 96);
+/
+
+
+
+function add(a,b)
+{
+    return a+b;
+}
+let result = add(1,2);
+console.log(result);
+*
+
+function add(a,b) {
+    return a + b;
+}
+let result = add(1,2);
+console.log(result);
+*
+
+function greet(name = "Guest"){
+console.log("Hello " + name + "");
+}
+greet("Alice");
+greet();
+*/
+
+const greet = name => ' Hello ${name}!' ;
+console.log(greet("Alice"));
