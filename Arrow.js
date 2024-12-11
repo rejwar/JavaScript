6c5efe8342bb392e6c89146b159ba0709a393ebd
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-11 17:49:11 +0600

    Create Arrow.js

---
+++ Arrow.js
@@ -0,0 +1,44 @@
+// const function = (parameter) => {
+    // code block function body 
+
+ // };
+
+
+//const function =(parameter) => {
+    // function body
+//};
+
+//const add = (a,b)=> a+b;
+//console.log(add(2,3));
+
+/*const add = (a,b )=> a+b;
+console.log(add(2,7));
+*
+
+const add = (a,b) => a+b;
+console.log(add(2,8));
+/
+
+const add = (a,b)=> a+b;
+console.log(add(2,70));
+
+
+
+const greet = () => console.log("Hello , world");
+greet();
+
+*
+
+const greet = () => console.log("Helllo world");
+greet();
+*
+
+
+const  greet = () => console.log("Hello world");
+greet();
+*/
+const greeet = () => console.log('object');
+greeet();
+
+const greet = () => console.log("Hi");
+greet();
