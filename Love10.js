a02b6cb40e4763da567d648cefdda53a8ff8119c
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-12 10:37:38 +0600

    Create Love10.js

---
+++ Love10.js
@@ -0,0 +1,35 @@
+/*
+const square = x => x *x;
+console.log(square(4));
+
+
+console.log(square(5));
+console.log(square(7));
+console.log(square(8));
+
+
+const multiply = (a,b) =>
+{
+    const result = a * b;
+    return result;
+}
+console.log(multiply(3,5));
+
+
+
+
+function Person (){
+    this.age = 0;
+    setInterval(() => {
+        this.age ++;
+        console.log(this.age);
+    }, 1000);
+}
+
+const p = new Person ();
+
+*/
+for(let i = 1; i<10 ; i++)
+    {
+        console.log("I love u ",i );
+    }
