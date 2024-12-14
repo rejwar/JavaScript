6f854e14a5aeb3f9f604f1bd6c491eaf11c100ec
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-14 22:06:02 +0600

    Update 10. Syntex.js

--- 10. Syntex.js
+++ 10. Syntex.js
@@ -24,3 +24,24 @@ function funcName(){
 }
 
 funcName();
+
+
+
+/*const x = 2;       //allow
+{
+    const x = 3;    //allow
+}
+
+
+{
+    const x = 5; // allow
+}
+*/
+
+
+
+/*let x = 5;
+let y = 5;
+let z = x+y;
+console.log(z); // 10 */
+
