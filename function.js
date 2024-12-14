0abf3dd46618122e1079862b5bf1fb08e90ba6ed
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-15 00:21:39 +0600

    Create Function.js

---
+++ function.js
@@ -0,0 +1,37 @@
+function myfunction(p1,p2){
+    return p1 * p2;
+}
+
+function name (parameter1, parameter2, parameter3){
+    //
+}
+
+let x = myfunction(4,3);
+
+function myFunction(a,b)
+{
+// Functions returns product of a and b.
+}
+
+function toCelsius(farenheit){
+    return (5/9) * (farenheit-32);
+}
+
+let value = toCelsius(77);
+
+function toCelsius(farenheit){
+    return (5/9) * (farenheit-32);
+}
+
+let value = toCelsius(77);  //
+
+
+let x = toCelsius(77);
+let text = "abcdefghijklmnopqrstuvwxyz";
+
+
+
+
+function myFunction(){
+    let carName = "Volvo";
+}
