65ddfcecd4673f1e451d48a31c4ba82b881dc381
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-15 14:33:07 +0600

    Create Srinagar.js

---
+++ Srinagar.js
@@ -0,0 +1,13 @@
+const user ={
+    id :1,
+    name : "shaista",
+    address:{
+        city : "Srinagar",
+        country : "India"
+    }
+};
+
+console.log(user.address.city);
+person.name = "hide"
+
+
