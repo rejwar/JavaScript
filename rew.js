58e2933816efb65944795b8dd5680831792d540e
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2024-12-15 14:36:46 +0600

    Create rew.js

---
+++ rew.js
@@ -0,0 +1,15 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet(){
+        console.log(`Hello ${this.name}`);
+    }
+}
+
+const person2 = new Person('Diana', 28);
+console.log(person2.age `${age}`);
+person2.greet();
+
