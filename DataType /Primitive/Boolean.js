13e79e206bf9557d0232d23f0808052357b6ca82
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-11-21 00:46:15 +0600

    new code

---
+++ DataType /Primitive/Boolean.js
@@ -0,0 +1,12 @@
+let Balance = 120;
+let AnotherBalance = new Number(120);
+
+console.log(typeof Balance);
+console.log(typeof AnotherBalance);
+
+
+
+// boolean 
+let isActive = true;
+let IsReallyActive = new Boolean(true);
+
