1dc230c64f021679598de189b30c3642e7c9feb4
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-11-21 01:05:36 +0600

    ne

--- DataType /Primitive/Boolean.js
+++ DataType /Primitive/Boolean.js
@@ -11,3 +11,12 @@ let isActive = true;
 let IsReallyActive = new Boolean(true);
 
 let UserName = "Hitesh";
+
+
+let OldGreet = myString + "" + "Rejwar ";
+console.log(OldGreet);
+
+let myString = "Hello ";
+
+let NewGreet = `${myString} ${UserName}`;
+console.log(NewGreet);
