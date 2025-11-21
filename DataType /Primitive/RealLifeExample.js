fe982ec54160c814073f53c663aaa9608de0a873
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-11-21 12:33:53 +0600

    new code

---
+++ DataType /Primitive/RealLifeExample.js
@@ -0,0 +1,15 @@
+// let price = [100, 200 ,150, 300];
+
+// let discount = price.map(p => p * 0.1);
+
+// console.log(discount);
+
+
+// //
+
+
+
+let nums = [10, 20, 30, 40, 50];
+let even = nums.filter(n => n % 20 === 0);
+
+console.log(even); // [20, 40]
