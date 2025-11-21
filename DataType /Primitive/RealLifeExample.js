32ffce706806cf348c12899833e67bdc7cc79f2a
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-11-21 14:33:43 +0600

    New

--- DataType /Primitive/RealLifeExample.js
+++ DataType /Primitive/RealLifeExample.js
@@ -9,7 +9,23 @@
 
 
 
-let nums = [10, 20, 30, 40, 50];
-let even = nums.filter(n => n % 20 === 0);
+// let nums = [10, 20, 30, 40, 50];
+// let even = nums.filter(n => n % 20 === 0);
 
-console.log(even); // [20, 40]
+// console.log(even); // [20, 40]
+
+
+
+// let price = [100, 200 ,150, 300];
+// let cheap = price.find(p => p < 160);
+
+
+
+// console.log(cheap); // 100
+
+
+let a = [5 ,10 ,15 ,50 , 60];
+
+let result = a.find(n => n > 20);
+
+console.log(result); // 50
