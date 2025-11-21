75894187b0991a155f2d310e5f5fbe00edc27789
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-11-21 15:18:09 +0600

    new code

--- DataType /ForEach.js
+++ DataType /ForEach.js
@@ -1,7 +1,14 @@
-let nums = [10, 20, 30, 40, 50];
+// let nums = [10, 20, 30, 40, 50];
 
-let a = nums.forEach(n => {
-    console.log(n * 2);
+// let a = nums.forEach(n => {
+//     console.log(n * 2);
+// });
+
+// console.log(a); // undefined    
+
+
+
+let a = [2,3 ,5];
+a.forEach(x => {
+    console.log(x *2);
 });
-
-console.log(a); // undefined    
---
+++ DataType /Include.js
@@ -0,0 +1,7 @@
+let nums = [10, 20, 30, 40, 50];
+
+nums.includes(20);
+nums.includes(100);
+
+console.log(nums.includes(20));  // true
+console.log(nums.includes(100)); // false
