1829ba446c21ef9b460590654d7e14e9dfea8952
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-11-21 11:24:51 +0600

    New code

---
+++ DataType /Primitive/NestedObject.js
@@ -0,0 +1,22 @@
+let user = {
+    name: "Alice",
+    location: {
+        city: "Wonderland",
+        zip: "12345"
+    }
+};
+
+let data = {
+    info: {
+        country: "Fictionland",
+    }
+};
+
+
+console.log(user.location.city); // Accessing nested object property
+console.log(data.info.country);  // Accessing nested object property
+
+//push
+
+Array.push(value)
+a.push(10);
